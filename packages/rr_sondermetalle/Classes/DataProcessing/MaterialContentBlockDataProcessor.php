dd11b2ead1f9b9a4c04a41663f34ed4cdfa81b81
Bita Sadeghi <bita.s9170@gmail.com>
2024-09-18 21:50:41 +0200

    fixed header & removed auth-buttons

--- packages/rr_sondermetalle/Classes/DataProcessing/MaterialContentBlockDataProcessor.php
+++ packages/rr_sondermetalle/Classes/DataProcessing/MaterialContentBlockDataProcessor.php
@@ -39,7 +39,7 @@ class MaterialContentBlockDataProcessor implements DataProcessorInterface
         $processedData['products'] = $result->fetchAllAssociative();
         $processedData['count'] = count($processedData['products']);
 
-        debug($processedData);
+        // debug($processedData);
 
         return $processedData;
     }
