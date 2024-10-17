1d7f81f3c45ee12cb6f9f240e96d5038da01bf79
jwmaryam <jw.maryamsadeghi@gmail.com>
2024-10-17 19:45:48 +0200

    added related-products content-element and get parameter from data proccessing

--- packages/rr_sondermetalle/Classes/DataProcessing/CardContentBlockDataProcessor.php
+++ packages/rr_sondermetalle/Classes/DataProcessing/CardContentBlockDataProcessor.php
@@ -4,9 +4,10 @@ declare(strict_types=1);
 
 namespace Romminger\RrSondermetalle\DataProcessing;
 
+use Symfony\Component\Yaml\Yaml;  
 use TYPO3\CMS\Frontend\ContentObject\ContentObjectRenderer;
 use TYPO3\CMS\Frontend\ContentObject\DataProcessorInterface;
-
+use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
 
 class CardContentBlockDataProcessor implements DataProcessorInterface
 {
@@ -16,11 +17,37 @@ class CardContentBlockDataProcessor implements DataProcessorInterface
         array $processorConfiguration,
         array $processedData
     ) {
+        
         if (isset($processorConfiguration['if.']) && !$cObj->checkIf($processorConfiguration['if.'])) {
             return $processedData;
         }
 
-        // debug($processedData['data']);
+        
+        $yamlFile = ExtensionManagementUtility::extPath('rr_sondermetalle') . 'Configuration/Yaml/interface.yaml';
+
+       
+        if (file_exists($yamlFile)) {
+            
+            $yamlData = Yaml::parseFile($yamlFile);
+
+            
+            $viewCounts = [];
+            $itemType = null;
+
+            foreach ($yamlData as $section) {
+                if ($section['identifier'] === 'view_count') {
+                    $viewCounts = $section['items'];
+                }
+
+                if ($section['identifier'] === 'item_type') {
+                    $itemType = $section['default'];
+                }
+            }
+
+        
+            $processedData['viewCounts'] = $viewCounts;
+            $processedData['itemType'] = $itemType;
+        }
 
         return $processedData;
     }
