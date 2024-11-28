04b4a6a40343114c348dfa9b9a68c7c48a2d2a67
Sadegh Saedi Nia <s.saedinia@gmail.com>
2024-11-28 10:01:58 +0100

    [#91] Added dashboard layout

---
+++ packages/rr_sondermetalle/Classes/DataProcessing/UserDataProcessor.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Romminger\RrSondermetalle\DataProcessing;
+
+use TYPO3\CMS\Core\Resource\FileReference;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Extbase\Mvc\Web\Routing\UriBuilder;
+use TYPO3\CMS\Frontend\ContentObject\ContentObjectRenderer;
+use TYPO3\CMS\Frontend\ContentObject\DataProcessorInterface;
+
+class UserDataProcessor implements DataProcessorInterface
+{
+    public function process(
+        ContentObjectRenderer $cObj,
+        array $contentObjectConfiguration,
+        array $processorConfiguration,
+        array $processedData
+    ): array {
+        if (!empty($GLOBALS['TSFE']->fe_user->user)) {
+            $user = $GLOBALS['TSFE']->fe_user->user;
+
+            if (!empty($user['image'])) {
+                $fileRepository = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Resource\FileRepository::class);
+                $fileObjects = $fileRepository->findByRelation('fe_users', 'image', $user['uid']);
+
+                if (!empty($fileObjects)) {
+                    /** @var FileReference $fileObject */
+                    $fileObject = reset($fileObjects);
+                    $user['avatar'] = $fileObject;
+                } else {
+                    $user['avatarFallback'] = $user['first_name'][0] . $user['last_name'][0];
+                }
+            }
+
+            $processedData['user'] = $user;
+        } else {
+            $processedData['user'] = null;
+        }
+
+        return $processedData;
+    }
+}
