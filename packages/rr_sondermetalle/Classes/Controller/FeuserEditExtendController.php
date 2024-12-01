e70a8467d6354288f968fdad5541c1bc63ed209b
Sadegh Saedi Nia <s.saedinia@gmail.com>
2024-12-01 21:27:45 +0100

    [#96] Account Dashboard Optimization

---
+++ packages/rr_sondermetalle/Classes/Controller/FeuserEditExtendController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Romminger\RrSondermetalle\Controller;
+
+use TYPO3\CMS\Core\Http\HtmlResponse;
+use Psr\Http\Message\ResponseInterface;
+use TYPO3\CMS\Core\Http\RedirectResponse;
+use Evoweb\SfRegister\Domain\Model\FrontendUser;
+use Evoweb\SfRegister\Controller\FeuserEditController;
+
+class FeuserEditExtendController extends FeuserEditController
+{
+
+    public function formAction(FrontendUser $user = null): ResponseInterface
+    {
+        parent::formAction($user);
+
+        switch ($this->request->getUri()->getPath()) {
+            case '/address':
+                $header = 'editAddress';
+                break;
+            case '/image':
+                $header = 'editImage';
+                break;
+            default:
+                $header = 'editProfile';
+                break;
+        }
+        $this->view->assign('header', $header);
+
+
+        return new HtmlResponse($this->view->render());
+    }
+
+    public function saveAction(FrontendUser $user): ResponseInterface
+    {
+
+        $response = parent::saveAction($user);
+
+        $pageUid = 40; // Dashboard Page
+        $url = $this->uriBuilder
+            ->reset()
+            ->setTargetPageUid($pageUid)
+            ->build();
+
+        return new RedirectResponse($url);
+    }
+}
